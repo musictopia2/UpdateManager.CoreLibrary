d5f3af1612ff573c832acbde85a3def018fa97e2
Andy Walter <musictopia2@hotmail.com>
2024-11-29 07:48:42 -0700

    Added dotnet upgrade model.

---
+++ YearlyNetUpgradeHelpers/Models/DotNetVersionUpgradeModel.cs
@@ -0,0 +1,10 @@
+﻿namespace UpdateManager.CoreLibrary.YearlyNetUpgradeHelpers.Models;
+public class DotNetVersionUpgradeModel
+{
+    public int NetVersion { get; set; }
+    public DateOnly LastUpdated { get; set; } //time does not matter.
+    public bool IsTestMode { get; set; }  // Determines if it's in test mode
+    public string TestLocalFeedPath { get; set; } = "";  // Path to the test local feed
+    public string TestPublicFeedPath { get; set; } = "";  // Path to the test public feed
+    public string ProdPrivateFeedPath { get; set; } = "";  // Path to the private production feed
+}
