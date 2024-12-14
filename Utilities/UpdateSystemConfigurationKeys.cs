33fcf9d66114abb9b3732af319ce2722f73d724e
Andy Walter <musictopia2@hotmail.com>
2024-12-14 13:08:56 -0700

    Fixed bug with private package path.

--- Utilities/UpdateSystemConfigurationKeys.cs
+++ Utilities/UpdateSystemConfigurationKeys.cs
@@ -7,7 +7,7 @@ public static class UpdateSystemConfigurationKeys
     // Key for configuration file path (e.g., path to the NuGet.config or other config files)
     public static string PackageConfigFileKey => "PackageConfigFileKey";
     // Key for configuration file path for the Private Nuget Packages
-    public static string PrivatePackageKey => "PackagePackageKey";
+    public static string PrivatePackageKey => "PrivatePackageKey";
     public static string DevelopmentPackageKey => "DevelopmentPackageKey";
     public static string StagingPackageKey => "StagingPackageKey";
     public static string PrefixKey => "PrefixKey";
