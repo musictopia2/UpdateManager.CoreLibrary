7a497f48a6e9098a605b5fc4352cafa270150e0f
Andy Walter <musictopia2@hotmail.com>
2024-12-27 06:54:23 -0700

    Improved the app model so i can specify a category.

--- Models/AppModel.cs
+++ Models/AppModel.cs
@@ -7,5 +7,6 @@ public class AppModel
     public string NetworkedPath { get; set; } = "";
     public string CsProj { get; set; } = "";
     public bool WindowsWPFBlazor { get; set; }
+    public string Category { get; set; } = ""; //this is used so i can specify to update only a category of apps.
     public bool Deleted { get; set; } //if i mark as deleted, then won't update.  however, would still show up so when discovering apps, won't add again.
 }
