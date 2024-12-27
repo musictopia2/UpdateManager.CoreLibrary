cd15da2eaaef99bc156c1be9a784441156f5c856
Andy Walter <musictopia2@hotmail.com>
2024-12-27 06:10:12 -0700

    Added abstractions for shortcuts.

---
+++ Interfaces/IShortcutManager.cs
@@ -0,0 +1,6 @@
+﻿namespace UpdateManager.CoreLibrary.Interfaces;
+public interface IShortcutManager
+{
+    BasicList<ShortcutModel> ListShortcuts();
+    void UpdateShortcut(string name, string newTargetPath);
+}
---
+++ Models/ShortcutModel.cs
@@ -0,0 +1,6 @@
+﻿namespace UpdateManager.CoreLibrary.Models;
+public class ShortcutModel
+{
+    public string Name { get; set; } = "";
+    public string TargetPath { get; set; } = "";
+}
