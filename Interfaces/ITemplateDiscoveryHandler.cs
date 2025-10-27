de873251e205e3c5cf87f73baf145a4f98166f04
Andy Walter <musictopia2@hotmail.com>
2025-10-27 14:11:11 -0700

    Removed the csharp editor from editor services.

--- Interfaces/ITemplateDiscoveryHandler.cs
+++ Interfaces/ITemplateDiscoveryHandler.cs
@@ -4,7 +4,7 @@ public interface ITemplateDiscoveryHandler
     Task<BasicList<string>> GetTemplateDirectoriesAsync();
     bool CanIncludeProject(string projectPath);
     void CustomizePackageModel(NuGetTemplateModel template);
-    bool NeedsPrefix(NuGetTemplateModel template, CsProjEditor editor);
+    bool NeedsPrefix(NuGetTemplateModel template);
     //this is needed though.
     EnumFeedType GetFeedType(string projectPath);
 }
