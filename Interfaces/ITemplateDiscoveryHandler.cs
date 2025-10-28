e34b802405e1c85c601e14ea75ad36808a5ffbe8
Andy Walter <musictopia2@hotmail.com>
2025-10-27 17:37:42 -0700

    Renamed some variable names.

--- Interfaces/ITemplateDiscoveryHandler.cs
+++ Interfaces/ITemplateDiscoveryHandler.cs
@@ -2,9 +2,9 @@
 public interface ITemplateDiscoveryHandler
 {
     Task<BasicList<string>> GetTemplateDirectoriesAsync();
-    bool CanIncludeProject(string projectPath);
+    bool CanIncludeProject(string directoryPath);
     void CustomizePackageModel(NuGetTemplateModel template);
     bool NeedsPrefix(NuGetTemplateModel template);
     //this is needed though.
-    EnumFeedType GetFeedType(string projectPath);
+    EnumFeedType GetFeedType(string directoryPath);
 }
