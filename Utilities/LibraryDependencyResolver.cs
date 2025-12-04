150413edd61ac8dcdec8c81d0bd3004bcd822c82
Andy Walter <musictopia2@hotmail.com>
2025-12-03 18:22:57 -0700

    Converted to using the new extension model in c#.

--- Utilities/LibraryDependencyResolver.cs
+++ Utilities/LibraryDependencyResolver.cs
@@ -1,77 +1,100 @@
 ﻿namespace UpdateManager.CoreLibrary.Utilities;
-//decided to make this public so 2 different libraries can use this.
 public static class LibraryDependencyResolver<T>
     where T : ILibraryUpgradeModel, new()
 {
-    public static BasicList<T> ResolveDependencies(BasicList<T> libraries)
+    public static BasicList<T> ResolveDependencies(
+        BasicList<T> libraries,
+        Func<string, string>? normalizer = null)
     {
         // Process the dependencies for each library
         foreach (T library in libraries)
         {
-            library.Dependencies = GetDependencies(library, libraries);
-            library.Dependencies.RemoveAllOnly(x => x == library.PackageName); // Can't depend on itself.
+            library.Dependencies = GetDependencies(library, libraries, normalizer);
+            library.Dependencies.RemoveAllOnly(x =>
+                StringComparer.OrdinalIgnoreCase.Equals(x, Normalize(library.PackageName, normalizer)));
         }
-        FinishScan(libraries);
-        // Return the updated list with dependencies populated
+
+        FinishScan(libraries, normalizer);
+
         return libraries;
     }
 
+    private static string Normalize(string name, Func<string, string>? normalizer)
+    {
+        return normalizer?.Invoke(name) ?? name;
+    }
+
     // Helper method to get dependencies from XML for each library
-    private static BasicList<string> GetDependencies(T library, BasicList<T> list)
+    private static BasicList<string> GetDependencies(
+        T library,
+        BasicList<T> list,
+        Func<string, string>? normalizer)
     {
         XElement source = XElement.Load(library.CsProjPath);
         BasicList<string> dependencies = [];
+
         var packageReferences = source.Descendants("PackageReference")
                                       .Where(x => x.Attribute("Include") != null)
                                       .Select(x => x.Attribute("Include")!.Value!)
                                       .ToBasicList();
-        foreach (var depName in packageReferences)
+
+        string normalizedLibraryName = Normalize(library.PackageName, normalizer);
+
+        foreach (var rawName in packageReferences)
         {
-            if (!StringComparer.OrdinalIgnoreCase.Equals(depName, library.PackageName) &&
-                list.Any(l => StringComparer.OrdinalIgnoreCase.Equals(l.PackageName, depName)))
+            string depName = Normalize(rawName, normalizer);
+
+            if (StringComparer.OrdinalIgnoreCase.Equals(depName, normalizedLibraryName))
+                continue;
+
+            if (list.Any(l =>
+                StringComparer.OrdinalIgnoreCase.Equals(
+                    Normalize(l.PackageName, normalizer),
+                    depName)))
             {
                 dependencies.Add(depName);
             }
         }
+
         return dependencies;
     }
-    private static void FinishScan(BasicList<T> list)
+
+    private static void FinishScan(
+        BasicList<T> list,
+        Func<string, string>? normalizer)
     {
         bool changesMade;
-        // Create a dictionary for faster lookup by library name (ignoring case)
-        var libraryDict = list.ToDictionary(l => l.PackageName, StringComparer.OrdinalIgnoreCase);
 
-        // Continue scanning until no more changes are made (i.e., all dependencies are resolved)
+        // Create dictionary using NORMALIZED names
+        var libraryDict = list.ToDictionary(
+            l => Normalize(l.PackageName, normalizer),
+            StringComparer.OrdinalIgnoreCase);
+
         do
         {
             changesMade = false;
 
-            // Iterate through each library in the list
             foreach (var library in list)
             {
-                // To hold new dependencies that need to be added to the current library
                 var newDependencies = new List<string>();
 
-                // For each dependency in the library's Dependencies list
                 foreach (var dep in library.Dependencies)
                 {
-                    // Now we need to resolve indirect dependencies by looking at other libraries in the list
-                    if (libraryDict.TryGetValue(dep, out var depLibrary))
+                    string normalizedDep = Normalize(dep, normalizer);
+
+                    if (libraryDict.TryGetValue(normalizedDep, out var depLibrary))
                     {
-                        // If the dependency library has its own dependencies, we need to add them too
                         foreach (var indirectDep in depLibrary.Dependencies)
                         {
-                            // Only add indirect dependencies if they are not already in the current library's list
                             if (!library.Dependencies.Contains(indirectDep, StringComparer.OrdinalIgnoreCase))
                             {
                                 newDependencies.Add(indirectDep);
-                                changesMade = true; // Mark that a change was made
+                                changesMade = true;
                             }
                         }
                     }
                 }
 
-                // Add the new dependencies to the library's Dependencies list
                 foreach (var newDep in newDependencies)
                 {
                     if (!library.Dependencies.Contains(newDep, StringComparer.OrdinalIgnoreCase))
@@ -82,6 +105,6 @@ public static class LibraryDependencyResolver<T>
                 }
             }
 
-        } while (changesMade); // Repeat the process until no changes are made
+        } while (changesMade);
     }
 }
