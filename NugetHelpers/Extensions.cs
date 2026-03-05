4e5861969f768ddb43c17d328b7cea5bc1a31e1a
Andy Walter <musictopia2@hotmail.com>
2026-03-05 10:47:18 -0700

    Added new extension to figure out if its a local feed or networked one.

--- NugetHelpers/Extensions.cs
+++ NugetHelpers/Extensions.cs
@@ -8,5 +8,36 @@ public static class Extensions
             string filePath = await ff1.GetSpecificFileAsync(nugetPath, ".nupkg");
             return filePath;
         }
-    }  
+
+    }
+    extension(string value)
+    {
+        public bool IsLocalFeed
+        {
+            get
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new CustomBasicException("Must have a value");
+                }
+
+                // UNC path: \\server\share\...
+                if (value.StartsWith(@"\\", StringComparison.OrdinalIgnoreCase))
+                {
+                    return true;
+                }
+
+                // Drive path: C:\... or C:/...
+                if (value.Length >= 3 &&
+                    char.IsLetter(value[0]) &&
+                    value[1] == ':' &&
+                    (value[2] == '\\' || value[2] == '/'))
+                {
+                    return true;
+                }
+
+                return false;
+            }
+        }
+    }
 }
